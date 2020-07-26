adf4fcac15cbeaa756741a18c4b4894f7534d8f9
Guilherme Tavares <guilhermetavalves@gmail.com>
2020-07-26 19:31:33 -0300

    adds globalStyles in project

---
+++ src/pages/App.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from '../styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado</title>
+        <link rel="shortcut icon" href="/img/icon0512.png" />
+        <link rel="apple-touch-icon" href="/img/icon0512.png" />
+        <meta name="description" content="Curso de React" />
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default App
